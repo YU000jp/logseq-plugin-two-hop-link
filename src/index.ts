cdad69701e3d01d91fa40d8871b682114997fa41
YU <111847207+YU000jp@users.noreply.github.com>
2025-06-08 18:11:18 +0900

    feat(): Not Support Logseq DB model #21

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,28 @@ import { loadTwoHopLink } from './hopLinks'
 import { setup as l10nSetup } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
 import ja from "./translations/ja.json"
 import { settings } from './settings'
+import { AppInfo } from '@logseq/libs/dist/LSPlugin.user'
+
+let logseqVersion: string = "" //バージョンチェック用
+let logseqVersionMd: boolean = false //バージョンチェック用
+// export const getLogseqVersion = () => logseqVersion //バージョンチェック用
+export const booleanLogseqVersionMd = () => logseqVersionMd //バージョンチェック用
 
 /* main */
 const main = async () => {
+
+  // バージョンチェック
+  logseqVersionMd = await checkLogseqVersion()
+  // console.log("logseq version: ", logseqVersion)
+  // console.log("logseq version is MD model: ", logseqVersionMd)
+  // 100ms待つ
+  await new Promise(resolve => setTimeout(resolve, 100))
+
+  if (logseqVersionMd === false) {
+    // Logseq ver 0.10.*以下にしか対応していない
+    logseq.UI.showMsg("The ’2 Hop Link’ plugin only supports Logseq ver 0.10.* and below.", "warning", { timeout: 5000 })
+    return
+  }
 
   await l10nSetup({ builtinTranslations: { ja } })
   /* user settings */
@@ -19,5 +38,25 @@ const main = async () => {
   })
 
 }/* end_main */
+
+// MDモデルかどうかのチェック DBモデルはfalse
+const checkLogseqVersion = async (): Promise<boolean> => {
+  const logseqInfo = (await logseq.App.getInfo("version")) as AppInfo | any
+  //  0.11.0もしくは0.11.0-alpha+nightly.20250427のような形式なので、先頭の3つの数値(1桁、2桁、2桁)を正規表現で取得する
+  const version = logseqInfo.match(/(\d+)\.(\d+)\.(\d+)/)
+  if (version) {
+    logseqVersion = version[0] //バージョンを取得
+    // console.log("logseq version: ", logseqVersion)
+
+    // もし バージョンが0.10.*系やそれ以下ならば、logseqVersionMdをtrueにする
+    if (logseqVersion.match(/0\.([0-9]|10)\.\d+/)) {
+      logseqVersionMd = true
+      // console.log("logseq version is 0.10.* or lower")
+      return true
+    } else logseqVersionMd = false
+  } else logseqVersion = "0.0.0"
+  return false
+}
+
 
 logseq.ready(main).catch(console.error)
